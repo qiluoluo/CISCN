ffea933011c6b46036696b8464c65eabda4f6895
Ki1ro <38156264+qiluoluo@users.noreply.github.com>
2023-03-15 00:24:06 +0800

    Update app.py

--- front/app.py
+++ front/app.py
@@ -1,97 +1,113 @@
-import os
-import sys
+import json
 
-# Flask
-from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from flask import Flask, render_template, url_for, redirect, request
+from flask_wtf.form import FlaskForm
+from flask_wtf import CSRFProtect
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
-# TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
-
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-
-# Some utilites
-import numpy as np
-from util import base64_to_pil
+import os
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 
 
-# Declare a flask app
-app = Flask(__name__)
+UPLOAD_PATH = os.getcwd()
+CHECK_PATH = os.path.dirname(os.getcwd())
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config["SECRET_KEY"] = "ABCDFWA"
+csrf = CSRFProtect(app)
 
 
-# You can use pretrained model from Keras
-# Check https://keras.io/applications/
-# or https://www.tensorflow.org/api_docs/python/tf/keras/applications
-
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-model = MobileNetV2(weights='imagenet')
-
-print('Model loaded. Check http://127.0.0.1:5000/')
+class UploadData(FlaskForm):
+    photo = FileField(label='图片上传', validators=[FileRequired(), FileAllowed(['jpg', 'png'])])
+    submit = SubmitField("提交")
 
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/your_model.h5'
-
-# Load your own trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-
-def model_predict(img, model):
-    img = img.resize((224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='tf')
-
-    preds = model.predict(x)
-    return preds
+@app.route('/', methods=['GET', 'POST'])
+def index():  # 主页面
+    x = UploadData()
+    if request.method == "GET":
+        return render_template("cover.html", up=x)
+    else:
+        pichead = x.photo.data
+        pichead.save(os.path.join(UPLOAD_PATH, "static", "pic", "PIC.jpg"))
+        return redirect(url_for("index2"))
 
 
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
+@app.route('/main', methods=['GET', 'POST'])
+def index2():
+    return render_template('databash.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'POST':
-        # Get the image from post request
-        img = base64_to_pil(request.json)
+def pre():
+    if request.method == 'GET':
+        from backend.model import model
+        from backend.predict import predict
+        from backend.heatmap import cam
+        from backend.windows import windows
+        # 例子
+        # model_name = 'tsnet-815'
+        #
+        # training_set = "FF++"
+        # # FF++ celeb-df-v2
+        # training_forgery_type = "deepfakes"
+        # # deepfakes neuraltextures face2face faceshifter faceswap all testdata None
+        # training_compression = "c23"
+        # # c23 c40 None
+        # testing_set = "FF++"
+        # # FF++ celeb-df-v2
+        # testing_forgery_type = "deepfakes"
+        # # deepfakes neuraltextures face2face faceshifter faceswap all testdata None
+        # testing_compression = "c23"
+        # # c23 c40 None
+        # checkpoint_save_path = CHECK_PATH + '/backend/output/checkpoint/{0}_{1}({2})--{3}_{4}({5})/{6}/'.format(training_set,
+        #                                                                                     training_forgery_type,
+        #                                                                                     training_compression,
+        #                                                                                     testing_set,
+        #                                                                                     testing_forgery_type,
+        #                                                                                     testing_compression,
+        #                                                                                     model_name)
+        model_name = request.args.get('model_name')
 
-        # Save the image to ./uploads
-        # img.save("./uploads/image.png")
+        training_set = request.args.get('model_name')
+        # FF++ celeb-df-v2
+        training_forgery_type = request.args.get('training_forgery_type')
+        # deepfakes neuraltextures face2face faceshifter faceswap all testdata None
+        training_compression = request.args.get('training_compression')
+        # c23 c40 None
+        testing_set = request.args.get('testing_set')
+        # FF++ celeb-df-v2
+        testing_forgery_type = request.args.get('testing_forgery_type')
+        # deepfakes neuraltextures face2face faceshifter faceswap all testdata None
+        testing_compression = request.args.get('testing_compression')
+        # c23 c40 None
+        checkpoint_save_path = CHECK_PATH + '/backend/output/checkpoint/{0}_{1}({2})--{3}_{4}({5})/{6}/'.format(training_set,
+                                                                                            training_forgery_type,
+                                                                                            training_compression,
+                                                                                            testing_set,
+                                                                                            testing_forgery_type,
+                                                                                            testing_compression,
+                                                                                            model_name)
+        image_path = os.path.join(UPLOAD_PATH, "static", "pic", "PIC.jpg")
+        # 获取模型
+        model = model(checkpoint_save_path)
+        # 获取预测结果参数
+        predict, pred = predict(model, image_path)
+        real_rate = "{:.8f}".format(predict.item()*100)
+        real = True if pred == 1 else False
+        # 获取热力图和自增强图
+        save_path = os.path.join(UPLOAD_PATH, "static", "pic")
+        cam_pic_path = cam(checkpoint_save_path, image_path, save_path)
+        # 获取敏感板块
+        win_pic_path = windows(checkpoint_save_path, image_path, save_path)
 
-        # Make prediction
-        preds = model_predict(img, model)
+        info_dict = {}
+        info_dict["real_rate"] = real_rate
+        info_dict["real"] = real
+        info_dict["cam_pic_path"] = cam_pic_path
+        info_dict["win_pic_path"] = win_pic_path
 
-        # Process your result for human
-        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-
-        result = str(pred_class[0][0][1])               # Convert to string
-        result = result.replace('_', ' ').capitalize()
-        
-        # Serialize the result, you can add additional fields
-        return jsonify(result=result, probability=pred_proba)
-
-    return None
+        return json.dumps(info_dict, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    app.run(port=5002, threaded=False)
-
-    # Serve the app with gevent
-    # http_server = WSGIServer(('0.0.0.0', 5000), app)
-    # http_server.serve_forever()
+    app.run(port=3690, debug=False)
